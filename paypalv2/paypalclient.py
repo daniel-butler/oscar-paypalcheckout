e3e12430a2c39e4cbd00eb248410ad3f7a2bb362
Daniel Butler <dabutler89@gmail.com>
2020-06-20 23:26:08 -0400

    cleaning up the client. Renamed primittive to primitive

--- paypalv2/paypalclient.py
+++ paypalv2/paypalclient.py
@@ -13,6 +13,11 @@ PAYPAL_DEBUG = getattr(settings, 'PAYPAL_DEBUG', True)
 
 
 class PayPalClient:
+    """
+    Returns PayPal HTTP client instance with environment that has access credentials context.
+    Use this instance to invoke PayPal APIs, provided the credentials have access.
+    """
+
     def __init__(self):
         self.client_id = PAYPAL_CLIENT_ID
         self.client_secret = PAYPAL_CLIENT_SECRET
@@ -21,29 +26,28 @@ class PayPalClient:
         if PAYPAL_ENVIRONMENT == 'live':
             self.environment = LiveEnvironment(client_id=self.client_id, client_secret=self.client_secret)
         else:
-
             self.environment = SandboxEnvironment(client_id=self.client_id, client_secret=self.client_secret)
 
-        """ Returns PayPal HTTP client instance with environment that has access
-            credentials context. Use this instance to invoke PayPal APIs, provided the
-            credentials have access. """
         self.client = PayPalHttpClient(self.environment)
 
-    def object_to_json(self, json_data):
+    def object_to_json(self, json_data) -> dict:
         """
         Function to print all json data in an organized readable manner
         """
         result = {}
+
+        # @todo: what does this do?
         if sys.version_info[0] < 3:
             itr = json_data.__dict__.iteritems()
         else:
             itr = json_data.__dict__.items()
-        for key,value in itr:
+
+        for key, value in itr:
             # Skip internal attributes.
             if key.startswith("__") or key.startswith("_"):
                 continue
             result[key] = self.array_to_json_array(value) if isinstance(value, list) else\
-                          self.object_to_json(value) if not self.is_primittive(value) else\
+                          self.object_to_json(value) if not self.is_primitive(value) else\
                           value
         return result
 
@@ -51,9 +55,9 @@ class PayPalClient:
         result =[]
         if isinstance(json_array, list):
             for item in json_array:
-                result.append(self.object_to_json(item) if not self.is_primittive(item)
+                result.append(self.object_to_json(item) if not self.is_primitive(item)
                               else self.array_to_json_array(item) if isinstance(item, list) else item)
         return result
 
-    def is_primittive(self, data):
+    def is_primitive(self, data):
         return isinstance(data, str) or isinstance(data, str) or isinstance(data, int)
---
+++ tests/unit/test_paypalclient.py
@@ -0,0 +1,10 @@
+import pytest
+
+
+def test_default_settings_are_set_as_expected():
+    # GIVEN the settings
+
+    # WHEN
+
+    # THEN
+    pytest.fail('not completed!')
