1edd8b5f4560434a5882a290e946e9e4b278320f
Daniel Butler <dabutler89@gmail.com>
2020-06-21 00:22:36 -0400

    remove extra spaces

--- paypalv2/urls.py
+++ paypalv2/urls.py
@@ -5,9 +5,6 @@ from .views import PaypalCancelView, PaypalSuccessView
 app_name = 'paypalcheckout'
 
 urlpatterns = [
-
     path('success/<int:orderno>',PaypalSuccessView.as_view(), name='paypalsucess'),
     path('cancel/<int:orderno>',PaypalCancelView.as_view(), name='paypalcancel'),
-
-
 ]
