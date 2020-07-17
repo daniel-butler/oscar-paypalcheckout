ac5d2433b5c7bfb7a25eece6f024d8ce58c6b119
dabuter89@gmail.com <Oct18ea20!>
2020-07-17 18:06:15 -0400

    fixed namespace issue

--- tests/urls.py
+++ tests/urls.py
@@ -1,10 +1,12 @@
-from django.urls import path
+from django.apps import apps
+from django.urls import include, path
 
 from paypalv2.views import PaypalCancelView, PaypalSuccessView
 
 app_name = 'paypalcheckout'
 
 urlpatterns = [
-    path('success/<int:orderno>',PaypalSuccessView.as_view(), name='paypalsuccess'),
-    path('cancel/<int:orderno>',PaypalCancelView.as_view(), name='paypalcancel'),
+    path('success/<int:orderno>', PaypalSuccessView.as_view(), name='paypalsuccess'),
+    path('cancel/<int:orderno>', PaypalCancelView.as_view(), name='paypalcancel'),
+    path('', include(apps.get_app_config('oscar').urls[0])),
 ]
